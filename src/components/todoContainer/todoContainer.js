67b3b4c07bbeae99b86f895574f49797dd48a2fa
Praveen Kumar Verma <vermapraween6543@gmail.com>
2022-07-03 20:26:40 +0530

    successfully uploading data to firebase

--- src/components/todoContainer/todoContainer.js
+++ src/components/todoContainer/todoContainer.js
@@ -2,7 +2,7 @@ import Button from "../button/Button";
 import Todo from "../todo/todo";
 import classes from "./todoContainer.module.css";
 import { useSelector, useDispatch } from "react-redux";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { taskListSliceActions } from "../../store/todo-store";
 import Empty from "./empty";
 
@@ -37,41 +37,69 @@ const TodoContainer = () => {
     dispatch(taskListSliceActions.deleteTask());
   };
 
+  useEffect(() => {
+    const uploadData = async () => {
+      if(todoArray.length === 0) return;
+      const response = await fetch(
+        "https://react-http-5b66d-default-rtdb.firebaseio.com/todo.json",
+        {
+          method: "PUT",
+          headers: { "Content-type": "application/json" },
+          body: JSON.stringify(todoArray),
+        }
+      );
+      const reply = await response.json();
+      console.log(reply);
+    };
+
+    uploadData();
+  }, [todoArray, completedTask, incompleteTask]);
+
   return (
     <div
       className={`${classes.todoContainer} ${
         !theme && classes.todoContainerDark
       }`}
     >
-      {all && (todoArray.length === 0
-        ? <Empty >No Task exist. Please add some task. 😀</Empty>
-        : todoArray.map((obj) => (
+      {all &&
+        (todoArray.length === 0 ? (
+          <Empty>No Task exist. Please add some task. 😀</Empty>
+        ) : (
+          todoArray.map((obj) => (
             <Todo
               key={obj.id}
               id={obj.id}
               description={obj.description}
               isCompleted={obj.completed}
             />
-          )))}
-      {complete && (completedTask.length === 0 ?
-        <Empty>No Task is been completed yet 😐. </Empty>:
-        completedTask.map((obj) => (
-          <Todo
-            key={obj.id}
-            id={obj.id}
-            description={obj.description}
-            isCompleted={true}
-          />
-        )))}
-      {incomplete && ( incompleteTask.length === 0 ? <Empty>Congratulations 👏 you've finished all of your task.</Empty> :
-        incompleteTask.map((obj) => (
-          <Todo
-            key={obj.id}
-            id={obj.id}
-            description={obj.description}
-            isCompleted={false}
-          />
-        )))}
+          ))
+        ))}
+      {complete &&
+        (completedTask.length === 0 ? (
+          <Empty>No Task is been completed yet 😐. </Empty>
+        ) : (
+          completedTask.map((obj) => (
+            <Todo
+              key={obj.id}
+              id={obj.id}
+              description={obj.description}
+              isCompleted={true}
+            />
+          ))
+        ))}
+      {incomplete &&
+        (incompleteTask.length === 0 ? (
+          <Empty>Congratulations 👏 you've finished all of your task.</Empty>
+        ) : (
+          incompleteTask.map((obj) => (
+            <Todo
+              key={obj.id}
+              id={obj.id}
+              description={obj.description}
+              isCompleted={false}
+            />
+          ))
+        ))}
 
       {todoArray.length > 0 && (
         <div
